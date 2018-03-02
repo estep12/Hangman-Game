99818b6c06e5ba20cb7ce971e862124dd0b02567
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-01 20:00:55 -0600

    Added header and styled header. Started to write javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+function resetGame(){
+
+}
+
+var words = [
+    "monkey",
+    "snake",
+    "tiger",
+    "frog",
+    "sloth"
+]
+
+var randomWord = words[Math.floor(Math.random() * words.length)];
