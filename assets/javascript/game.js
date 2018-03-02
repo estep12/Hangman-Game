e0c5260367c6d701249f580dde5a4741b0aea7a5
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-01 21:51:04 -0600

    added javascript to get random words generated. Added style to css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,34 @@
-function resetGame(){
+// function resetGame(){
 
-}
+// }
+
+let wins = 0;
+
+document.getElementById("wins").innerHTML=wins;
+
 
 var words = [
     "monkey",
     "snake",
     "tiger",
     "frog",
-    "sloth"
-]
+    "sloth",
+    "lizard"
+];
 
 var randomWord = words[Math.floor(Math.random() * words.length)];
+
+console.log(randomWord);
+
+
+var answer = [];
+for (let i = 0; i<randomWord.length; i++) {
+    answer[i] = "_";
+}
+let remainingLength = randomWord.length;
+
+document.getElementById("currentword").innerHTML=answer;
+
+let guessesRemaining = 15;
+
+document.getElementById("remainingguesses").innerHTML = guessesRemaining;
