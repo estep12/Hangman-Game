c4ca9e0cbd6c7a554e72f26e6a8fc3f8d01de7b2
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-04 15:26:28 -0600

    Got correct letters to display correctly, wins to add up, and remaining guesses to decrease

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,58 +32,54 @@ var remainingLength = randomWord.length;
 
 document.getElementById("currentword").innerHTML=answerArray.join("");
 
-let guessesRemaining = 15;
+var guessesRemaining = 15;
 
 document.getElementById("remainingguesses").innerHTML = guessesRemaining;
+console.log(guessesRemaining);
 
 
 // functions
 
-// function updateScore() {
-//     document.querySelector("#wins").innerHTML = "Wins: " + score;
-//   }
 
 
 
-
-    document.onkeyup = function(event) {
+    document.onkeypress = function(event) {
         var letter = String.fromCharCode(event.which).toLowerCase();
-        // var guess = KeyboardEvent.key;
-        // console.log(guess);
-        // var answerArray = [];
-        // for (let i = 0; i<randomWord.length; i++) {
-        // answerArray[i] = "  _  ";
-        // }
+        guessesRemaining --;
+        document.getElementById("remainingguesses").innerHTML = guessesRemaining;
+      
         for (let j = 0; j<randomWord.length; j++){
-        console.log(letter);
-    
-        if(randomWord[j] === letter) {
-            answerArray[i] == letter;
-            remainingLength--;
-            var key = document.getElementById("currentword").innerHTML;
-            var replaceLetter = key.replace("_",letter);
-            document.getElementById("currentword").innerHTML = replaceLetter;
+        
+            if(randomWord[j] === letter) {
+                answerArray[j] = letter;
+                document.getElementById("currentword").innerHTML=answerArray.join("")
+                remainingLength--; 
+                console.log(remainingLength);
+                if (remainingLength === 0) {
+                    wins++;
+                    document.getElementById("wins").innerHTML=wins;
+                    if(remainingLength === 0){
+                        break;
+                    }
+                }
+            } 
 
-            // if(guess === randomWord.length){
-            //     for( var j = 0; j <randomWord.length; j++){
-            //         if(randomWord[j] === guess){
-            //             answerArray[i] === guess;
-            //             remainingLength--;
-            //             var key = document.getElementById("currentword").innerHTML;
-            //             var replaceLetter = key.replace("_",letter);
-            //             document.getElementById("currentword").innerHTML = replaceLetter;
-            //         }
-            //     }
+            
+
+            if (j+1 === randomWord.length){
+                // answerArray.length != letter;
+                var missedLetter = document.getElementById("guessedletters").innerHTML;
+                var wrongLetter = missedLetter.replace(' ', missedLetter);
+                document.getElementById("guessedletters").innerHTML = wrongLetter;
+                // alert('ur  gay');
+
+
             // }
-        } else if (randomWord[j] != letter){
-            answerArray[i] != letter;
-            var missedLetter = document.getElementById("guessedletters").innerHTML;
-            document.getElementById("guessedletters").innerHTML = missedLetter;
-        }
-    
+                
+                
+        
         }
 
     }
 
-// Process
-
+    }
