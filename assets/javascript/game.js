0a508626e4b88d968882334371eb42bc27ad9ef3
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-04 12:47:33 -0600

    Added functions to javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,18 +16,21 @@ var words = [
     "lizard"
 ];
 
+
 var randomWord = words[Math.floor(Math.random() * words.length)];
 
 console.log(randomWord);
 
 
-var answer = [];
+var answerArray = [];
 for (let i = 0; i<randomWord.length; i++) {
-    answer[i] = "  _  ";
+    answerArray[i] = "  _  ";
 }
-let remainingLength = randomWord.length;
+var remainingLength = randomWord.length;
 
-document.getElementById("currentword").innerHTML=answer.join("");
+
+
+document.getElementById("currentword").innerHTML=answerArray.join("");
 
 let guessesRemaining = 15;
 
@@ -36,21 +39,51 @@ document.getElementById("remainingguesses").innerHTML = guessesRemaining;
 
 // functions
 
-function updateScore() {
-    document.querySelector("#wins").innerHTML = "Wins: " + score;
-  }
+// function updateScore() {
+//     document.querySelector("#wins").innerHTML = "Wins: " + score;
+//   }
+
+
+
+
+    document.onkeyup = function(event) {
+        var letter = String.fromCharCode(event.which).toLowerCase();
+        // var guess = KeyboardEvent.key;
+        // console.log(guess);
+        // var answerArray = [];
+        // for (let i = 0; i<randomWord.length; i++) {
+        // answerArray[i] = "  _  ";
+        // }
+        for (let j = 0; j<randomWord.length; j++){
+        console.log(letter);
+    
+        if(randomWord[j] === letter) {
+            answerArray[i] == letter;
+            remainingLength--;
+            var key = document.getElementById("currentword").innerHTML;
+            var replaceLetter = key.replace("_",letter);
+            document.getElementById("currentword").innerHTML = replaceLetter;
+
+            // if(guess === randomWord.length){
+            //     for( var j = 0; j <randomWord.length; j++){
+            //         if(randomWord[j] === guess){
+            //             answerArray[i] === guess;
+            //             remainingLength--;
+            //             var key = document.getElementById("currentword").innerHTML;
+            //             var replaceLetter = key.replace("_",letter);
+            //             document.getElementById("currentword").innerHTML = replaceLetter;
+            //         }
+            //     }
+            // }
+        } else if (randomWord[j] != letter){
+            answerArray[i] != letter;
+            var missedLetter = document.getElementById("guessedletters").innerHTML;
+            document.getElementById("guessedletters").innerHTML = missedLetter;
+        }
+    
+        }
+
+    }
 
 // Process
-document.onkeyup = function(event) {
-    for (let i = 0; i<randomWord.length; i++){
-    var letter = String.fromCharCode(event.which).toLowerCase();
-    console.log(letter);
 
-    if(letter === answer[i]);{
-        var key = document.getElementById("currentword").innerHTML;
-        var replaceLetter = key.replace("_",letter);
-        document.getElementById("currentword").innerHTML = replaceLetter;
-    }
-    
-    }
-}
